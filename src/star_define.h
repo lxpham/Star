42bef34729b7fab032b42098db021911e4416e24
lxpham <phamxuanlai@gmail.com>
2015-12-03 00:40:29 +0700

    star_define.h - Change pins assignment for Star rev.03
    Change pins assignment for Star rev.03

--- src/star_define.h
+++ src/star_define.h
@@ -23,37 +23,37 @@
 #include "bbb_define.h"
 
 /*GPIO Input definition*/
-#define GPIO_IN_0 		BBB_GPIO0_23  /*P8_13*/
-#define GPIO_IN_1 		BBB_GPIO1_14  /*P8_16*/
-#define GPIO_IN_2 		BBB_GPIO1_15  /*P8_15*/
-#define GPIO_IN_3 		BBB_GPIO2_1   /*P8_18*/
-#define GPIO_IN_4 		BBB_GPIO2_3   /*P8_8*/
-#define GPIO_IN_5		  BBB_GPIO2_2   /*P8_7*/
-#define GPIO_IN_6		  BBB_GPIO2_4   /*P8_10*/
-#define GPIO_IN_7		  BBB_GPIO2_5   /*P8_9*/
-#define GPIO_IN_8		  BBB_GPIO0_27  /*P8_17*/
-#define GPIO_IN_9		  BBB_GPIO0_22  /*P8_19*/   
-#define GPIO_IN_10		BBB_GPIO1_29  /*P8_26*/
-#define GPIO_IN_11		BBB_GPIO2_24  /*P8_28*/
-#define GPIO_IN_12		BBB_GPIO1_12  /*P8_12*/
-#define GPIO_IN_13		BBB_GPIO1_13  /*P8_11*/ 
-#define GPIO_IN_14		BBB_GPIO0_26  /*P8_14*/   
+#define GPIO_IN_0 		BBB_GPIO0_27  	/*P8_17*/
+#define GPIO_IN_1 		BBB_GPIO1_14  	/*P8_16*/
+#define GPIO_IN_2 		BBB_GPIO1_15  	/*P8_15*/
+#define GPIO_IN_3 		BBB_GPIO0_26   	/*P8_14*/
+#define GPIO_IN_4 		BBB_GPIO2_1   	/*P8_18*/
+#define GPIO_IN_5		BBB_GPIO0_22   	/*P8_9*/
+#define GPIO_IN_6		BBB_GPIO1_29   	/*P8_26*/
+#define GPIO_IN_7		BBB_GPIO2_24    /*P8_28*/
+#define GPIO_IN_8		BBB_GPIO2_2     /*P8_7*/
+#define GPIO_IN_9		BBB_GPIO2_3     /*P8_8*/
+#define GPIO_IN_10		BBB_GPIO2_5  	/*P8_9*/
+#define GPIO_IN_11		BBB_GPIO2_4     /*P8_10*/
+#define GPIO_IN_12		BBB_GPIO1_13    /*P8_11*/
+#define GPIO_IN_13		BBB_GPIO1_12    /*P8_12*/
+#define GPIO_IN_14		BBB_GPIO0_23    /*P8_13*/
 
 /*GPIO Output definition*/
-#define GPIO_OUT_0		BBB_GPIO2_11  /*P8_42*/
-#define GPIO_OUT_1		BBB_GPIO2_10  /*P8_41*/
-#define GPIO_OUT_2		BBB_GPIO2_13  /*P8_40*/
-#define GPIO_OUT_3		BBB_GPIO2_12  /*P8_39*/
-#define GPIO_OUT_4		BBB_GPIO2_23  /*P8_29*/
-#define GPIO_OUT_5		BBB_GPIO2_25  /*P8_30*/
-#define GPIO_OUT_6		BBB_GPIO2_22  /*P8_27*/
-#define GPIO_OUT_7		BBB_GPIO2_7   /*P8_46*/
-#define GPIO_OUT_8		BBB_GPIO2_6   /*P8_45*/
-#define GPIO_OUT_9		BBB_GPIO2_9   /*P8_44*/
-#define GPIO_OUT_10		BBB_GPIO2_8   /*P8_43*/
-#define GPIO_OUT_11		BBB_GPIO0_9   /*P8_33*/
-#define GPIO_OUT_12		BBB_GPIO2_17  /*P8_34*/
-#define GPIO_OUT_13		BBB_GPIO0_10  /*P8_31*/
+#define GPIO_OUT_0		BBB_GPIO2_12  /*P8_39*/
+#define GPIO_OUT_1		BBB_GPIO2_11  /*P8_42*/
+#define GPIO_OUT_2		BBB_GPIO2_10  /*P8_41*/
+#define GPIO_OUT_3		BBB_GPIO2_9   /*P8_44*/
+#define GPIO_OUT_4		BBB_GPIO2_8   /*P8_43*/
+#define GPIO_OUT_5		BBB_GPIO2_7   /*P8_46*/
+#define GPIO_OUT_6		BBB_GPIO2_6   /*P8_45*/
+#define GPIO_OUT_7		BBB_GPIO2_17  /*P8_34*/
+#define GPIO_OUT_8		BBB_GPIO0_9   /*P8_33*/
+#define GPIO_OUT_9		BBB_GPIO2_16  /*P8_36*/
+#define GPIO_OUT_10		BBB_GPIO0_8   /*P8_35*/
+#define GPIO_OUT_11		BBB_GPIO2_15  /*P8_38*/
+#define GPIO_OUT_12		BBB_GPIO2_14  /*P8_37*/
+#define GPIO_OUT_13		BBB_GPIO2_13  /*P8_40*/
 
 /*ADC definition*/
 #define ADC_IN_0		BBB_AIN0        /*P9_39*/
@@ -65,14 +65,14 @@
 #define ADC_IN_6		BBB_AIN6        /*P9_35*/
 
 /*LED definition*/
-#define RUN_LED			  BBB_GPIO0_7     /*P9_42*/   
-#define ERROR_LED		  BBB_GPIO3_19    /*P9_27*/
+#define RUN_LED			BBB_GPIO3_17    /*P9_28*/  /*Muxed with SPI1_CS0*/
+#define ERROR_LED		BBB_GPIO3_19    /*P9_27*/
 
-#define USER_LED_0		BBB_GPIO3_21    /*P9_25*/       
-#define USER_LED_1		BBB_GPIO1_17    /*P9_23*/
-#define USER_LED_2		BBB_GPIO0_3     /*P9_21*/ /*Mux with UART2_TXD*/  
-#define USER_LED_3		BBB_GPIO0_13    /*P9_19*/ /*Mux with I2C2_SCL*/
-#define USER_LED_4		BBB_GPIO1_28    /*P9_12*/
+#define USER_LED_0		BBB_GPIO2_22    /*P8_27*/
+#define USER_LED_1		BBB_GPIO2_25    /*P8_30*/
+#define USER_LED_2		BBB_GPIO2_23    /*P8_29*/ /*Mux with UART2_TXD*/
+#define USER_LED_3		BBB_GPIO0_11    /*P8_32*/ /*Mux with I2C2_SCL*/
+#define USER_LED_4		BBB_GPIO0_10    /*P8_31*/
 
 /*I2C definition*/
 #define I2C1_SCL		BBB_I2C1_SCL    /*P9_17*/
@@ -81,13 +81,26 @@
 /*RS485 definition*/
 #define RS485_TX		BBB_UART1_TXD   /*P9_24*/
 #define RS485_RX		BBB_UART1_RXD   /*P9_26*/
-#define RS485_RW		BBB_GPIO1_16    /*P9_15*/
+#define RS485_RW		BBB_GPIO1_17    /*P9_23*/
 
 /*RS232 definition*/
 #define RS232_TX		BBB_UART4_TXD   /*P9_13*/
 #define RS232_RX		BBB_UART4_RXD   /*P9_11*/
 
 /*Speaker definition*/
-#define SPEAKER_OUT		BBB_GPIO0_11  /*P8_32*/
+#define SPEAKER_OUT		BBB_GPIO0_3  	/*P9_21*/ /*Muxed with UART2_TXD*/
+
+/*PWM*/
+
+#define PWM1A			BBB_EHRPWM1A   	/*P9_14*/
+#define PWM1B			BBB_EHRPWM1B	/*P9_16*/
+
+/*Reserved for future use*/
+
+#define _1C				BBB_I2C2_SDA	/*P9_20*/
+#define _1D				BBB_I2C2_SCL	/*P9_19*/
+#define _1E				BBB_UART2_RXD	/*P9_22*/
+
+
 
 #endif /* STAR_DEFINE_H_ */

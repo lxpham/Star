15ba93189581e0285efe1c32c912567034db20c2
lxpham <phamxuanlai@gmail.com>
2015-12-12 23:38:59 +0700

    Rearrange GPIO port into increasing order
    Rearrange GPIO port into increasing order

--- src/bbb_define.h
+++ src/bbb_define.h
@@ -9,37 +9,38 @@
 
 #ifndef BBB_DEFINE_H_
 #define BBB_DEFINE_H_
-
-
-#define		BBB_GPIO2_12	   76
-#define		BBB_GPIO2_11       75
-#define		BBB_GPIO2_10       74
-#define		BBB_GPIO2_9        73
-#define		BBB_GPIO2_8        72
-#define		BBB_GPIO2_7        71
-#define		BBB_GPIO2_6        70
-#define		BBB_GPIO2_17       81
-#define		BBB_GPIO0_9        09
-#define		BBB_GPIO2_16       80
+/*GPIO 0*/
 #define		BBB_GPIO0_8        08
-#define		BBB_GPIO2_15       79
-#define		BBB_GPIO2_14       78
-#define		BBB_GPIO2_13       77
+#define		BBB_GPIO0_9        09
+#define		BBB_GPIO0_22       22
+#define		BBB_GPIO0_23       23
+#define		BBB_GPIO0_26       26
 #define		BBB_GPIO0_27       27
+/*GPIO 1*/
+#define		BBB_GPIO1_12       44
+#define		BBB_GPIO1_13       45
 #define		BBB_GPIO1_14       46
 #define		BBB_GPIO1_15       47
-#define		BBB_GPIO0_26       26
-#define		BBB_GPIO2_1        65
-#define		BBB_GPIO0_22       22
 #define		BBB_GPIO1_29       61
-#define		BBB_GPIO2_24       88
+/*GPIO 2*/
+#define		BBB_GPIO2_1        65
 #define		BBB_GPIO2_2        66
 #define		BBB_GPIO2_3        67
-#define		BBB_GPIO2_5        69
 #define		BBB_GPIO2_4        68
-#define		BBB_GPIO1_13       45
-#define		BBB_GPIO1_12       44
-#define		BBB_GPIO0_23       23
+#define		BBB_GPIO2_5        69
+#define		BBB_GPIO2_6        70
+#define		BBB_GPIO2_7        71
+#define		BBB_GPIO2_8        72
+#define		BBB_GPIO2_9        73
+#define		BBB_GPIO2_10       74
+#define		BBB_GPIO2_11       75
+#define		BBB_GPIO2_12	   76
+#define		BBB_GPIO2_13       77
+#define		BBB_GPIO2_14       78
+#define		BBB_GPIO2_15       79
+#define		BBB_GPIO2_16       80
+#define		BBB_GPIO2_17       81
+#define		BBB_GPIO2_24       88
 
 ///*ADC Group*/
 //#define BBB_AIN0

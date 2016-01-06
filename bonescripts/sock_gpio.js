1a398456707d29d8ae1488c2d6aa2a8a88a6e156
lxpham <phamxuanlai@gmail.com>
2016-01-06 08:36:55 +0700

    Update sock_gpio.js
    Re-write socket for Input, the result is not disappear anymore.

--- bonescripts/sock_gpio.js
+++ bonescripts/sock_gpio.js
@@ -1,11 +1,11 @@
 // Remote control of BeagleBone USR LED
 // Server-side Node.JS code.
 // Jaakko Ala-Paavola, 2013-08-14
-
+var b = require('bonescript');
 var app = require('http').createServer(httpserver);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
-var b = require('bonescript');
+
  
 app.listen(8070);
 
@@ -13,15 +13,15 @@ var led = 'USR3';
 var input = "P8_19";
 var output = "P8_35";
  
-setup = function() {
-    b.pinMode(led, OUTPUT);
+
+    b.pinMode(led, b.OUTPUT);
     b.digitalWrite(led,b.HIGH);
     
-    b.pinMode(output, OUTPUT);
+    b.pinMode(output, b.OUTPUT);
     b.digitalWrite(output,b.HIGH);
     
-    b.pinMode(input,INPUT);
-};
+    b.pinMode(input,b.INPUT);
+
 
 function httpserver (req, res) {
   fs.readFile('gpio.html',
@@ -37,16 +37,15 @@ function httpserver (req, res) {
 }
 
 
+
+
 io.sockets.on('connection', function (socket) {
-  socket.on('input', function (data) {
-    console.log(data);
-    if (data == 'read'){
-        socket.emit('show', b.digitalRead(input));
-        socket.broadcast.emit('show', b.digitalRead(input));
-    }
-  });
+      socket.emit('show', b.digitalRead(input));
+});
   
-    socket.on('output', function (data) {
+  
+  io.sockets.on('connection', function (socket) {
+  socket.on('output', function (data) {
     console.log(data);
     if (data == 'on'){
         b.digitalWrite(output, b.HIGH); 
@@ -55,62 +54,5 @@ io.sockets.on('connection', function (socket) {
         b.digitalWrite(output, b.LOW); 
     }
   });
-});
-// Remote control of BeagleBone USR LED
-// Server-side Node.JS code.
-// Jaakko Ala-Paavola, 2013-08-14
-
-var app = require('http').createServer(httpserver);
-var io = require('socket.io').listen(app);
-var fs = require('fs');
-var b = require('bonescript');
- 
-app.listen(8070);
-
-var led = 'USR3';
-var input = "P8_19";
-var output = "P8_35";
- 
-setup = function() {
-    b.pinMode(led, OUTPUT);
-    b.digitalWrite(led,b.HIGH);
-    
-    b.pinMode(output, OUTPUT);
-    b.digitalWrite(output,b.HIGH);
-    
-    b.pinMode(input,INPUT);
-};
-
-function httpserver (req, res) {
-  fs.readFile('gpio.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading gpio.html');
-    }
- 
-    res.writeHead(200);
-    res.end(data);
   });
-}
 
-
-io.sockets.on('connection', function (socket) {
-  socket.on('input', function (data) {
-    console.log(data);
-    if (data == 'read'){
-        socket.emit('show', b.digitalRead(input));
-        socket.broadcast.emit('show', b.digitalRead(input));
-    }
-  });
-  
-    socket.on('output', function (data) {
-    console.log(data);
-    if (data == 'on'){
-        b.digitalWrite(output, b.HIGH); 
-    }
-    else {
-        b.digitalWrite(output, b.LOW); 
-    }
-  });
-});

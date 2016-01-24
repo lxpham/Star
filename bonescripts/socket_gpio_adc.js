2ed98cd3561244f9c97a9bd6c1ea8dba77363e46
lxpham <phamxuanlai@gmail.com>
2016-01-25 00:20:55 +0700

    Create socket_gpio_adc.js

---
+++ bonescripts/socket_gpio_adc.js
@@ -0,0 +1,61 @@
+// Remote control of BeagleBone USR LED
+// Server-side Node.JS code.
+// Jaakko Ala-Paavola, 2013-08-14
+var b = require('bonescript');
+var app = require('http').createServer(httpserver);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+ 
+app.listen(8070);
+
+var led = 'USR3';
+var input = "P8_19";
+var output = "P8_35";
+var adcInput ="P9_36";
+
+    b.pinMode(led, b.OUTPUT);
+    b.digitalWrite(led,b.HIGH);
+    
+    b.pinMode(output, b.OUTPUT);
+    b.digitalWrite(output,b.HIGH);
+    
+    b.pinMode(input,b.INPUT);
+
+
+function httpserver (req, res) {
+  fs.readFile('gpio-adc.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading gpio.html');
+    }
+ 
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+
+
+
+io.sockets.on('connection', function (socket) {
+      socket.emit('show', b.digitalRead(input));
+});
+  
+io.sockets.on('connection', function (socket) {
+      socket.emit('adc-show', b.analogRead(adcInput));
+});
+
+  io.sockets.on('connection', function (socket) {
+  socket.on('output', function (data) {
+    console.log(data);
+    if (data == 'on'){
+        b.digitalWrite(output, b.HIGH); 
+    }
+    else {
+        b.digitalWrite(output, b.LOW); 
+    }
+  });
+  });
+

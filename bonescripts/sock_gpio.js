f23d051be8255f2f6e0660dbfaa08e7a63c6ed0e
lxpham <phamxuanlai@gmail.com>
2016-01-25 12:35:40 +0700

    Update sock_gpio.js
    Change button type from "submit" to "button" for preventing refresh all web page

--- bonescripts/sock_gpio.js
+++ bonescripts/sock_gpio.js
@@ -40,8 +40,12 @@ function httpserver (req, res) {
 
 
 io.sockets.on('connection', function (socket) {
-      socket.emit('show', b.digitalRead(input));
+    socket.on('input', function (data) { 
+        console.log(data);
+        socket.emit('show', b.digitalRead(input));
+    });
 });
+  
   
   
   io.sockets.on('connection', function (socket) {
